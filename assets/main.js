87b745d0be8b87782de241f24f9b96b8649054dc
NelsR <nels@Nelss-iMac.local>
2017-02-06 12:01:15 -1000

    Updated step 5

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,12 @@ $(function() {
 
   // your code will go here
 
+      $.ajax({
+    url: 'https://www.codeschool.com/users/3276827.json,
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
+    
 });
