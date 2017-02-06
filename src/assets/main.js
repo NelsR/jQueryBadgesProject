066729d241ea048456ae125c2af22db43431c649
NelsR <nels@Nelss-iMac.local>
2017-02-06 12:37:06 -1000

    Change to append

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,7 +9,7 @@ $(function() {
             success: function(response) {
               // handle response
                for(i = 0; i < response.courses.completed.length; i++){
-                    $("#badges").prepend("<div class='course'><h3>"+response.courses.completed[i].title+"</h3><img src='"+response.courses.completed[i].badge+"'/><a href='"+response.courses.completed[i].url+"' class='btn btn-primary' target='_blank'>See Course</a>");
+                    $("#badges").append("<div class='course'><h3>"+response.courses.completed[i].title+"</h3><img src='"+response.courses.completed[i].badge+"'/><a href='"+response.courses.completed[i].url+"' class='btn btn-primary' target='_blank'>See Course</a>");
                 } 
 
             }
